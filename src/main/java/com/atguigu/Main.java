4d601f97cf91ed3c01a3d1fe98e0f82eaf1ab5d6
wzf-PerformanceBook <2971434037@qq.com>
2024-10-16 14:10:07 +0800

    测试网络

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -19,6 +19,7 @@ public class Main {
     }
 
     //配置mybatis-plus插件
+
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
@@ -26,6 +27,7 @@ public class Main {
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());  //乐观锁
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());  //防全局修改和删除
         return interceptor;
+
     }
 
 }
